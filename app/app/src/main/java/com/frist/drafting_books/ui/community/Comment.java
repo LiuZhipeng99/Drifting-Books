d3ce2461786d6c0452af779d8d93c56198f70977
unknown <1438714538@qq.com>
2021-04-09 10:30:45 +0800

    实现评论功能，但是没有和数据库结合的版本

--- app/app/src/main/java/com/frist/drafting_books/ui/community/Comment.java
+++ app/app/src/main/java/com/frist/drafting_books/ui/community/Comment.java
@@ -1,42 +1,110 @@
 package com.frist.drafting_books.ui.community;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.frist.drafting_books.MainActivity;
 import com.frist.drafting_books.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import cn.leancloud.AVUser;
 
 public class Comment extends AppCompatActivity {
-
+    private Dialog dialog;
+    private RecyclerView recyclerView;
+    private CommentAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
         initComment();
+        Button bt_comment=findViewById(R.id.bt_comment);
+        bt_comment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showReplyDialog();
+            }
+        });
     }
+
+    private void showReplyDialog() {
+        dialog = new BottomSheetDialog(this);
+        View commentView = LayoutInflater.from(this).inflate(R.layout.comment_dialog_layout,null);
+        final EditText commentText = (EditText) commentView.findViewById(R.id.dialog_comment_et);
+        final Button bt_comment = (Button) commentView.findViewById(R.id.dialog_comment_bt);
+        commentText.setHint("评论:");
+        dialog.setContentView(commentView);
+        bt_comment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String replyContent = commentText.getText().toString().trim();
+                if(!TextUtils.isEmpty(replyContent)){
+
+
+                    dialog.dismiss();
+                    //这里把用户的名字传进去。
+                    AVUser curUser = AVUser.getCurrentUser();
+                    Map<String,String> map=new HashMap<>();
+                    //等实现登录功能先。
+//                    map.put("name",curUser.get("name").toString());
+                    //注意，adapter用的键值是reader comment
+                    map.put("reader","liyunwei");
+                    map.put("comment",replyContent);
+
+                    adapter.addTheReplyData(map);
+                    Toast.makeText(Comment.this,"回复成功",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(Comment.this,"回复内容不能为空",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        dialog.show();
+    }
+
     void initComment(){
-        RecyclerView recyclerView=findViewById(R.id.comment);
-        List<String> list=new ArrayList<>();
-        list.add("版权归作者所有，任何形式转载请联系作者。\n" +
-                "作者：风君（来自豆瓣）\n" +
-                "来源：https://book.douban.com/review/5799892/\n" +
-                "\n" +
-                "\n" +
-                "《人间失格》书成当年，太宰治旋即投水自尽。这部遗作，也因此在太宰的作品之中占有举足轻重的地位，被认为是作家一生遭遇与心路历程的映射。史铁生曾说过：“写作者，未必能塑造出真实的他人，写作者只可能塑造真实的自己。”这话用在太宰治身上可谓恰如其分，纵观他的各部作品中那诸多角色，不啻为他自己的无数分身。而在《人间失格》里，这种自我写照实在过于明显，以至于对太宰治略有了解的读者根本无需分析，就可以看出书中主角“大庭叶藏”其实就是“津岛修治”，亦即作者本人的化身。书中以叶藏独白道出的经历，与作者本人的人生重合度之高，令这部作品也被冠以“自传体小说”之名。鉴于其“遗作”的特殊地位，本书可看作是太宰治本人对自己人生的某种“总结”，窥探其内心世界的最后机会。在本书中，作者依旧一如既往地描写了一个被社会排斥的“边缘人”角色的挣扎与沉沦。而若要问本书与太宰其它作品相比最显著的特点，或者“相同之中的些许不同”是什么的话，恐怕只能说，本作是刻画太宰治“丑角精神”最深入、最全面也最彻底的一部作品。\n");
-        list.add("版权归作者所有，任何形式转载请联系作者。\n" +
-                "作者：渔夫和鱼（来自豆瓣）\n" +
-                "来源：https://book.douban.com/review/1074604/\n" +
-                "\n" +
-                "\n" +
-                "然死生之事，于作家亦莫大焉。想象这样一个作家，一生以毁减生命为志业，从二十岁起五度自杀，终于在四十岁前了结生命；想象这样一个作家，毕生的写作都是以自身为蓝本，自传体式的回忆贯串文本；想象这样一个作家，生前毁誉参半，死去近六十年还被不断追忆，连忌日都成为节日…… 他就是太宰治，日本无赖派大师，在日本与川端康成，三岛由纪夫并列战后文学的巅峰人物。\n" +
-                "　　 一\n" +
-                "　　太宰治，本名津岛修治，1909年出生于日本青森县北津轻郡贵族家庭，父亲是当地首要人物，曾任众议院议员、贵族院议员，母亲体弱多病，不能亲自抚养孩子，所以在十个孩子中排行第九的太宰治自小他由姑母及保母照顾，父亲的严厉与母亲的缺位让太宰治从小心思纤细而敏感。太宰治在初中时后开始创办同人刊物，从此决心以文学为业。1930年至东京大学法文系就读，师从井伏鳟二。大学时期太宰治积极参加左翼运动，同时开始过着放浪不羁的生活，曾与艺妓同居，毕业后走向消极，其间四次殉情未遂，三十九岁时与最后一位爱人相约投水自尽。1935年太宰治以《丑角之舞》初登文坛，短篇《逆行》亦入围芥川奖，后来继续出版不少的作品集，其中尤其以晚期的《斜阳》与《人间失格》为人称道，被誉为战后日本文学的金字塔作品。");
-        CommentAdapter adapter=new CommentAdapter(this,list);
+        recyclerView=findViewById(R.id.comment);
+        //todo 在这里传入评论列表，并且可以传入作者名字。
+
+        List<Map<String,String>> list=new ArrayList<>();
+        Map<String,String> map1=new HashMap<>();
+        map1.put("reader","lyw");
+        map1.put("comment","版权归作者所有，任何形式转载请联系作者。\\n\" +\n" +
+                "                \"作者：风君（来自豆瓣）\\n\" +\n" +
+                "                \"来源：https://book.douban.com/review/5799892/\\n\" +\n" +
+                "                \"\\n\" +\n" +
+                "                \"\\n\" +\n" +
+                "                \"《人间失格》书成当年，太宰治旋即投水自尽。这部遗作，"+
+                "也因此在太宰的作品之中占有举足轻重的地位，被认为是作家一生遭遇与心路历程的映射。"+"" +
+                "史铁生曾说过：“写作者，未必能塑造出真实的他人，写作者只可能塑造真实的自己。”这话用在太宰治身上可谓恰如其分"+
+                "，纵观他的各部作品中那诸多角色，不啻为他自己的无数分身。而在《人间失格》里，这种自我写照实在过于明显，"+
+                "以至于对太宰治略有了解的读者根本无需分析，就可以看出书中主角“大庭叶藏”其实就是“津岛修治”，亦即作者本人的化身。"+
+                "书中以叶藏独白道出的经历，与作者本人的人生重合度之高，令这部作品也被冠以“自传体小说”之名。"+
+                "鉴于其“遗作”的特殊地位，本书可看作是太宰治本人对自己人生的某种“总结”，窥探其内心世界的最后机会。"+
+                "在本书中，作者依旧一如既往地描写了一个被社会排斥的“边缘人”角色的挣扎与沉沦。"+
+                "而若要问本书与太宰其它作品相比最显著的特点，或者“相同之中的些许不同”是什么的话，恐怕只能说，"+
+                "本作是刻画太宰治“丑角精神”最深入、最全面也最彻底的一部作品。\\n");
+        for(int i=0;i<10;i++){
+            list.add(map1);
+        }
+
+
+        adapter=new CommentAdapter(this,list);
         LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(linearLayoutManager);
--- app/app/src/main/java/com/frist/drafting_books/ui/community/CommentAdapter.java
+++ app/app/src/main/java/com/frist/drafting_books/ui/community/CommentAdapter.java
@@ -11,11 +11,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.frist.drafting_books.R;
 
 import java.util.List;
+import java.util.Map;
 
 public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.MyViewHolder> {
-    private List<String> commentList;
+    private List<Map<String,String>> commentList;
     private Context context;
-    public CommentAdapter(Context context, List<String> list) {
+    public CommentAdapter(Context context, List<Map<String,String>> list) {
         this.context=context;
         commentList=list;
     }
@@ -30,7 +31,9 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.MyViewHo
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-            holder.commentview.setContent(commentList.get(position));
+        //这里可能键值对不上.
+            holder.commentview.setTitle(commentList.get(position).get("reader"));
+            holder.commentview.setContent(commentList.get(position).get("comment"));
     }
 
     @Override
@@ -44,5 +47,9 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.MyViewHo
             commentview=itemView.findViewById(R.id.commentitem);
 
         }
+    }
+    public void addTheReplyData(Map<String,String> map){
+        commentList.add(0,map);
+        notifyDataSetChanged();//这个东西难道能提醒数据变动吗？
     }
 }
