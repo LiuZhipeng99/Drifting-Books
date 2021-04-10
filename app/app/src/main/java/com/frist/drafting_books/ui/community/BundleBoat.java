10ac9b21a07b80e9240f60bf87d6a1cd5db65e18
unknown <1438714538@qq.com>
2021-04-11 03:46:59 +0800

    实现个人页面书单详情

--- app/app/src/main/java/com/frist/drafting_books/ui/community/BundleBoat.java
+++ app/app/src/main/java/com/frist/drafting_books/ui/community/BundleBoat.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 import cn.leancloud.AVObject;
 //这个类用来作为传递适配器。把avobject放到bundle传输
 public class BundleBoat extends AVObject implements Serializable {
-    BundleBoat(AVObject oj){
+    public BundleBoat(AVObject oj){
         super(oj);
 
         System.out.println("gouzao");
